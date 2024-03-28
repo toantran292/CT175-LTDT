7cb9393c469a31a78eadf7fdf1618b3653807646
toantran292 <tranthaitoan998@gmail.com>
2024-03-29 00:36:12 +0700

    Chinh ty

--- tryhard/cau_13.c
+++ tryhard/cau_13.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
 #define oo 99999
+#define maxn 1000
 #define read(a) scanf("%d", &a)
 #define read2(a, b) scanf("%d%d", &a, &b)
 #define read3(a, b, c) scanf("%d%d%d", &a, &b, &c)
 #define read4(a, b, c, d) scanf("%d%d%d%d", &a, &b, &c, &d)
 #define debug(a) fprintf(stderr, "#DEBUG: %s = %d\n", #a, a)
-#define maxn 1000
 
-int n, m;
-int color[maxn];
-int a[maxn][maxn];
-int q[9999], f = -1, r = -1, flag = 1, cnt[2] = {0, 0};
+int color[maxn], a[maxn][maxn];
+int n, m, q[9999], f = -1, r = -1, cnt[2] = {0, 0}, u, v;
 
 void bfs(int u) {
   q[++r] = u;
   color[u] = 0;
   cnt[0]++;
 
-  while (f != r && flag) {
+  while (f < r) {
     int u = q[++f];
     for (int v = 1; v <= n; v++) {
       if (a[u][v]) {
         if (color[u] == color[v]) {
-          flag = 0;
+          f = 9999;
           return;
-        }
-        if (color[v] == -1) {
+        } else if (color[v] == -1) {
           color[v] = !color[u];
           cnt[color[v]]++;
           q[++r] = v;
@@ -38,28 +35,22 @@ void bfs(int u) {
 int main() {
   read2(n, m);
   for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= n; j++) {
+    color[i] = -1;
+    for (int j = 1; j <= n; j++)
       a[i][j] = 0;
-    }
   }
   for (int i = 0; i < m; i++) {
-    int u, v;
     read2(u, v);
-    a[u][v] = 1;
-    a[v][u] = 1;
+    a[u][v] = a[v][u] = 1;
   }
 
-  for (int i = 1; i <= n; i++)
-    color[i] = -1;
-
-  for (int u = 1; u <= n; u++) {
+  for (int u = 1; u <= n; u++)
     if (color[u] == -1)
       bfs(u);
-  }
 
-  if (flag)
-    printf("%d %d", cnt[0], cnt[1]);
-  else
-    printf("-1 -1");
+  if (f == 9999)
+    cnt[0] = cnt[1] = -1;
+  printf("%d %d", cnt[0], cnt[1]);
+
   return 0;
 }
