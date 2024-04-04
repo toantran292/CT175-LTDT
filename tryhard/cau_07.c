fc0a27a2244b4ad2ad05bc27c2e17fbc51b5135b
toantran292 <tranthaitoan998@gmail.com>
2024-04-05 06:31:39 +0700

    :))

--- tryhard/cau_07.c
+++ tryhard/cau_07.c
@@ -6,27 +6,24 @@ int n;
 int g[maxn][maxn];
 
 int main() {
-  scanf("%d", &n);
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= n; j++) {
-      scanf("%d", &g[i][j]);
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            scanf("%d", &g[i][j]);
+        }
     }
-  }
-  int u, v;
-  scanf("%d%d", &u, &v);
+    int u, v;
+    scanf("%d%d", &u, &v);
 
-  int cnt = 0;
-  for (int i = 1; i <= n; i++) {
-    if (i == u || i == v)
-      continue;
-    if (!g[u][i] && !g[i][u])
-      continue;
-    if (!g[v][i] && !g[i][v])
-      continue;
-    cnt++;
-    // printf("%d ", i);
-  }
-  printf("%d", cnt);
-  //   if (!cnt)
-  //     printf("KHONG CHUNG DOI THU");
+    int cnt = 0;
+    for (int i = 1; i <= n; i++) {
+        if (i == u || i == v) continue;
+        if (!g[u][i] && !g[i][u]) continue;
+        if (!g[v][i] && !g[i][v]) continue;
+        cnt++;
+        // printf("%d ", i);
+    }
+    printf("%d", cnt);
+    //   if (!cnt)
+    //     printf("KHONG CHUNG DOI THU");
 }
